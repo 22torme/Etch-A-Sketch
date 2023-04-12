4db133329fabede0b9a8aabb77d354b679a46a9c
Tyler <torme2828@gmail.com>
2023-04-11 21:22:12 -0500

    Functional drawing and color changing features added

--- script.js
+++ script.js
@@ -25,3 +25,58 @@ for (var i = 0; i < 16; i++) {
   // Append the row element to the container element
   container.appendChild(row);
 }
+
+//selecting all columns
+const columns = document.querySelectorAll('.col');
+console.log(columns);
+
+//-------------------------------------------------
+//get color from color picker
+const color = document.querySelector('#color');
+let colorValue = color.value;
+
+//add event listener to color picker
+color.addEventListener('change', () => {
+    colorValue = color.value;
+    columns.style.hover = colorValue;
+});
+//-------------------------------------------------
+
+
+
+
+//-------------------------------------------------
+//COLUMNS
+let mouseDown = false;
+
+columns.forEach((column) => {
+    column.addEventListener('mousedown', () => {
+        mouseDown = true;
+        });
+        column.addEventListener('mouseup', () => {
+            mouseDown = false;
+            });
+            column.addEventListener('mouseover', () => {
+                if (mouseDown) {
+                    column.style.backgroundColor = colorValue;
+                    column:hover = colorValue;
+                }
+            });
+        });
+
+columns.forEach((column) => {
+    column.addEventListener('click', () => {
+        column.style.backgroundColor = colorValue;
+    });
+});
+
+//-------------------------------------------------
+
+//RESET BUTTON
+const reset = document.querySelector('#reset');
+reset.addEventListener('click', () => {
+  columns.forEach((column) => {
+    column.style.backgroundColor = 'white';
+  });
+}
+);
